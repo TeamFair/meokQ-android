377ac2f989124dd10f2d7956e51badb382ff5798
kimwest00 <minseohufs@hufs.ac.kr>
2023-08-27 09:43:38 +0900

    feat:#2 login screen 구성

--- presentation/src/main/java/com/meokq/presentation/ui/signin/SigninActivity.kt
+++ presentation/src/main/java/com/meokq/presentation/ui/signin/SigninActivity.kt
@@ -1,32 +1,107 @@
 package com.meokq.presentation.ui.signin
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.meokq.presentation.R
 import com.meokq.presentation.getResourceId
+import com.meokq.presentation.theme.KakaoLoginButton
+import com.meokq.presentation.theme.MainPrimary
+import com.meokq.presentation.theme.White
 
 @Composable
 fun LoginButton(
-    img:String
-){
+    img: String,
+    backgroundColor: Color
+) {
     var imgUrl = "ic_login_${img}_18"
     Row(
-        horizontalArrangement = Arrangement.SpaceAround
+        modifier = Modifier
+            .shadow(elevation = 2.dp, spotColor = Color(0x40000000), ambientColor = Color(0x40000000))
+            .background(color = backgroundColor, shape = RoundedCornerShape(size = 6.dp))
+            .width(250.dp)
+            .height(50.dp),
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment = Alignment.CenterVertically
     ) {
         Image(
             painter = painterResource(
                 id = getResourceId(imgUrl, R.drawable::class.java),
             ),
-           contentDescription = "login logo"
+            contentDescription = "login logo"
         )
         Text(
             text = "Login with ${img}",
         )
         Text(
-            text = "",)
+            text = "",
+        )
     }
 }
+
+@Preview
+@Composable
+fun LoginScreen(
+
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .background(color = White),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+
+    ) {
+        Image(
+            modifier= Modifier
+                .width(150.dp)
+                .height(150.dp),
+            painter = painterResource(
+                R.drawable.iv_logo_150
+            ),
+
+            contentDescription = "login logo"
+        )
+        Text(
+            text = stringResource(R.string.application_name), style = TextStyle(
+                fontSize = 50.sp,
+                color = MainPrimary,
+                fontWeight = FontWeight.Bold
+            )
+        )
+        Spacer(modifier = Modifier.height(150.dp))
+        LoginButton(
+            img = "kakao",
+            backgroundColor = KakaoLoginButton
+        )
+        Spacer(modifier = Modifier.height(27.dp))
+        LoginButton(
+            img = "google",
+            backgroundColor = White
+        )
+    }
+}
+   
